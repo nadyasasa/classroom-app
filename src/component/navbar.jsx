3bce614b3312644e0a37a89a3910572767a25e2a
natashastia <natashaastia@mail.ugm.ac.id>
2023-06-03 10:58:29 +0700

    add user booking page

---
+++ src/component/navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Logo from "../assets/simaster.png"
+
+const navbar = () => {
+    return (
+        <div className="bg-white px-0 pt-5 ml-8">
+            <img src={Logo} alt="Logo"/>
+        </div>
+    )
+}
+
+export default navbar
--- src/pages/UserBook.jsx
+++ src/pages/UserBook.jsx
@@ -1,23 +1,111 @@
 import React from "react"
-import { BsFillPlusCircleFill } from "react-icons/bs";
+import Navbar from "../component/navbar.jsx";
 
+const rules = [
+    {description: 'Maksimal Peminjaman dilakukan dalam 1 hari sebelum penggunaan ruangan.'},
+    {description: 'Pengguna yang sudah memesan tapi tidak hadir, maka dikenakan sanksi pemblokiran sampai 3 hari. Kecuali melakukan pembatalan pemesanan maksimal 1 hari sebelum waktu jam buka pada periode dan hari yang ditentukan.'},
+    {description: 'Apabila mahasiswa melakukan 2 kali pemesanan kursi dalam 1 hari yang sama dan tidak hadir pada periode 1 tanpa pembatalan, maka sistem akan membatalkan pesanan periode 2 secara otomatis.'},
+    {description: 'Pemesanan tempat tidak dapat dilakukan pada waktu periode berjalan.'}
+  ];
 const UserBook = () => {
+    const listRules = rules.map(rules => 
+        <li>{rules.description}</li>
+    );
     return (
-        <div className="bg-[005575] h-screen flex pt-10 px-16 text-xl">
-            <div className="bg-white h-full flex flex-col gap-5 font-extrabold w-full pr-5 rounded-lg">
-                <div className="flex justify-between ml-5 mt-5">
-                    <div className="ml-1.5 mt-1">
-                        Daftar Peminjaman Ruangan
+        <div className="bg-blue h-screen flex py-5 px-16 text-xl">
+            <div className="bg-white h-full flex flex-col gap-5 w-full pr-5 rounded-lg">
+                <Navbar/>
+                <div className="bg-lightblue h-fit w-fit px-10 py-3 ml-8 mr-4 text-black border-solid border-2 border-blue">
+                    <h5 className="font-medium">
+                        Aturan Peminjaman oleh Pengguna
+                    </h5>
+                    <ol 
+                        style={{ listStyleType: 'decimal'}}
+                        className="pt-2 text-justify">
+                        {listRules}
+                    </ol>           
+                </div>
+                <div className="grid grid-cols-2 gap-x-16 gap-y-4 pb-5 items-center bg-graylight border-solid border-2 border-gray px-10 py-3 ml-8 mr-4">
+                    <div className="bw-full">
+                        <label>
+                            Nama Peminjam
+                        </label>
+                        <input
+                            type="text"
+                            id="name"
+                            className="w-full mt-2 bg-white border border-graydark rounded"
+                            >
+                        </input>
                     </div>
-                    <div className="flex justify-between">
-                        <button className="bg-black text-white flex font-normal text-sm rounded-lg px-2 py-1 items-center content-center">
-                            <BsFillPlusCircleFill />
-                            <div className="ml-2">Tambah Peminjaman</div>
-                        </button>
+                    <div className="bw-full">
+                        <label>
+                            Email UGM
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            className="w-full mt-2 bg-white border border-graydark rounded"
+                            >
+                        </input>
+                    </div>
+                    <div className="bw-full">
+                        <label>
+                            Keperluan
+                        </label>
+                        <input
+                            type="text"
+                            id="need"
+                            className="w-full mt-2 bg-white border border-graydark rounded"
+                            >
+                        </input>
+                    </div>
+                    <div className="bw-full">
+                        <label>
+                            Ruangan
+                        </label>
+                        <input
+                            type="text"
+                            id="classroom"
+                            className="w-full mt-2 bg-white border border-graydark rounded"
+                            >
+                        </input>
+                    </div>
+                    <div className="bw-full">
+                        <label>
+                            Tanggal Peminjaman
+                        </label>
+                        <input
+                            type="date"
+                            id="date"
+                            className="w-full mt-2 bg-white border border-graydark rounded"
+                            >
+                        </input>
+                    </div>
+                    <div className="bw-full">
+                        <label>
+                            Waktu Peminjaman
+                        </label>
+                        <div className="flex gap-2  items-center">
+                            <input
+                                type="time"
+                                id="time_start"
+                                className="w-full mt-2 bg-white border border-graydark rounded"
+                                >
+                            </input>
+                        s.d
+                            <input
+                                type="time"
+                                id="time_end"
+                                className="w-full mt-2 bg-white border border-graydark rounded"
+                                >
+                            </input>
+                        </div>
                     </div>
                 </div>
-                <div>
-                    Tabel
+                <div className="flex justify-center">
+                    <button className="h-fit w-fit bg-[#F4C522] text-white font py-2 px-4 flex rounded">
+                        Book Now
+                    </button>
                 </div>
             </div>
         </div>
